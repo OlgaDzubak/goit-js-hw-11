374af07305fe5745678a7b4204bd4f76ad187fc0
OlgaDzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-04-29 18:58:36 +0000

    Deploying to gh-pages from @ OlgaDzubak/goit-js-hw-11@12e7df29f79d267c114c33db7542cbd69063b4e2 🚀

---
+++ index.77151707.js
@@ -0,0 +1,2 @@
+const o=".search-form"|document.querySelector;console.log(o);
+//# sourceMappingURL=index.77151707.js.map
---
+++ index.fb0f8ede.js
@@ -0,0 +1,2 @@
+!function(){var o=".search-form"|document.querySelector;console.log(o)}();
+//# sourceMappingURL=index.fb0f8ede.js.map
